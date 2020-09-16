03522e0da62f59b006a35358a39cbd86fca84236
Michael Phelan <mphelan@g.harvard.edu>
2020-09-16 17:06:48 -0400

    django tutorial material in dataverse read application

---
+++ opendp/dvread/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DvreadConfig(AppConfig):
+    name = 'dvread'
--- opendp/opendp/settings.py
+++ opendp/opendp/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'dvread.apps.DvreadConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
